f5a3216e2552a38877c4537fc89324350c15a8f6
OmarHamed <amourhamed931@gmail.com>
2024-08-27 12:49:33 +0300

    need to choose between auth.user or my user

--- E_bookstore_API/user/models.py
+++ E_bookstore_API/user/models.py
@@ -1,17 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 class User(AbstractBaseUser):
     username = models.CharField(max_length=100, unique=True)
-   
+
     email = models.EmailField(max_length=100, unique=True)
-    password = models.CharField(max_length=50)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     id = models.AutoField(primary_key=True)
+    objects = UserManager()
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
     def __str__(self):
-        return self.email
+        return self.username
--- E_bookstore_API/user/serializers.py
+++ E_bookstore_API/user/serializers.py
@@ -1,15 +1,21 @@
 from operator import is_
 from rest_framework import serializers
 
+
 from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'password', 'username']
+        fields = ['username','email','password']
         extra_kwargs = {'password': {'write_only': True}}
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
         return user
+        
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
             raise serializers.ValidationError('Username is already taken')
