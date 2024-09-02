96bd3d0f90ee36d4a6fe1c54dfc57b1264d4e2b3
OmarHamed <amourhamed931@gmail.com>
2024-09-02 15:56:54 +0300

    Add Book model and upload_location function

--- E_bookstore_API/books/models.py
+++ E_bookstore_API/books/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+def upload_location(instance, filename):
+    return f"book_cover/{instance.title}/{filename}"
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    published_at = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    cover = models.ImageField(upload_to=upload_location, null=True, blank=True)
+
+    
+    def __str__(self):
+        return self.title
+    
 # Create your models here.
