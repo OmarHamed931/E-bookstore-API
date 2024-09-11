2ab79c94ff5dd7f1f44a5e0b6b846a44cdf2a2ae
Omar_Hamed <amourahamed931@gmail.com>
2024-09-11 06:13:28 +0300

    Refactor category URLs

--- E_bookstore_API/category/urls.py
+++ E_bookstore_API/category/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import CategoryList , CategoryNames , FeaturedBooks
+from .views import CategoryList , CategoryNames 
 
 urlpatterns = [
     path('', CategoryNames.as_view(), name='category-names'),
     path('<str:name>/', CategoryList.as_view(), name='category-list'),
-    path('featured/', FeaturedBooks.as_view(), name='featured-books')
+    
 
 ]
